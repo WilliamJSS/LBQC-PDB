d37572a4963f9184f3706a26fe2d564a42325785
WilliamJSS <williamsena088@gmail.com>
2023-03-07 17:52:16 -0300

    updated R script

--- peptides_analysis.R
+++ peptides_analysis.R
@@ -1,31 +1,34 @@
 # Download Packages
-install.packages("Peptides", dependencies=TRUE)
-install.packages("tidyverse")
-install.packages("ggpubr")
+#install.packages("Peptides", dependencies=TRUE)
+#install.packages("tidyverse")
+#install.packages("ggpubr")
 
 # Reading Rpeptides
 library(Peptides)
 
 # Importing and filtering dataset
 df <- read.csv("LBQCPDB.csv")
+#df <- df[1:5,]
+sequences <- as.character(df$Sequence)
 
 ## Peptides description ##
 
 # Molecular Weight and Charge correlation
-mz<- as.numeric(mz(df$Sequence))
+mz <- as.numeric(mz(sequences))
 df$mz <- mz
 
 # Charge
-ch <- as.numeric(charge(df$Sequence, pH = 7, pKscale = "Lehninger"))
+ch <- as.numeric(charge(sequences, pH = 7, pKscale = "Lehninger"))
 df$ch <- ch
 
+
 # Hydrophobicity
-hd <- as.numeric(hydrophobicity(df$Sequence, scale = "KyteDoolittle"))
+hd <- as.numeric(hydrophobicity(sequences, scale = "KyteDoolittle"))
 df$hd <- hd
 
 # Molecular Weight
 mw <- as.numeric(mw(
-  df$Sequence,
+  sequences,
   monoisotopic = FALSE,
   avgScale = "expasy",
   label = "none",
@@ -35,23 +38,23 @@ mw <- as.numeric(mw(
 df$mw <- mw
 
 # Compute the Boman (Potential Protein Interaction) index
-bw <- as.numeric(boman(df$Sequence))
+bw <- as.numeric(boman(sequences))
 df$bw <- bw
 
 # Plot regression
-library(tidyverse)
-library(ggpubr)
-theme_set(theme_pubr())
+#library(tidyverse)
+#library(ggpubr)
+#theme_set(theme_pubr())
 
 # Total Charge x Docking Score
-ggplot(df, aes(x = df$Docking.score..1st.model., y = df$ch)) +
-  geom_point() +
-  stat_smooth()
+#ggplot(df, aes(x = df$Docking.score..1st.model., y = df$ch)) +
+#  geom_point() +
+#  stat_smooth()
 
 # Total Molecular Weigth x Docking Score
-ggplot(df, aes(x = df$mw, y = df$Docking.score..1st.model.)) +
-  geom_point() +
-  stat_smooth()
+#ggplot(df, aes(x = df$mw, y = df$Docking.score..1st.model.)) +
+#  geom_point() +
+#  stat_smooth()
 
 # Recording new table
-write.csv(df,"LBQCPDB_descriptions.csv", row.names = TRUE, col.names = TRUE)
+#write.csv(df,"LBQCPDB_descriptions.csv", row.names = TRUE, col.names = TRUE)
