6fde590cf544271a50075d185d42d478d820c15f
WilliamJSS <williamsena088@gmail.com>
2023-03-13 17:58:38 -0300

    Add sequences analysis

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,19 +10,23 @@ def multi_replace(text, replaces) -> (str, dict):
     return text
 
 
+def path():
+    return f'{os.getcwd().split("/scripts")[0]}/PDBs_releases'
+
+
 def pd_to_csv(df, path, name):
     dt = datetime.now()
-    df.to_csv(f'{path}/releases/{name}_{dt.strftime("%Y_%m_%d")}.csv', index=False)
+    df.to_csv(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv', index=False)
     df.to_csv(f'{path}/{name}.csv', index=False)
     print('Save files in:')
-    print(f'{path}/releases/{name}_{dt.strftime("%Y_%m_%d")}.csv')
+    print(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv')
     print(f'{path}/{name}.csv')
     
     
 def pl_to_csv(df, path, name):
     dt = datetime.now()
-    df.write_csv(f'{path}/releases/{name}_{dt.strftime("%Y_%m_%d")}.csv')
+    df.write_csv(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv')
     df.write_csv(f'{path}/{name}.csv')
     print('Save files in:')
-    print(f'{path}/releases/{name}_{dt.strftime("%Y_%m_%d")}.csv')
+    print(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv')
     print(f'{path}/{name}.csv')
