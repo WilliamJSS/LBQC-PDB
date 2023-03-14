fd2c45ce54a13fbae00887d179e59b7e783ce737
WilliamJSS <williamsena088@gmail.com>
2023-03-14 10:30:17 -0300

    Update CancerPPD data

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,22 +11,22 @@ def multi_replace(text, replaces) -> (str, dict):
 
 
 def path():
-    return f'{os.getcwd().split("/scripts")[0]}/PDBs_releases'
+    return f'{os.getcwd().split("scripts")[0]}PDBs_releases'
 
 
 def pd_to_csv(df, path, name):
     dt = datetime.now()
-    df.to_csv(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv', index=False)
-    df.to_csv(f'{path}/{name}.csv', index=False)
+    df.to_csv(f'{path}\\{name}\\{name}_{dt.strftime("%Y_%m_%d")}.csv', index=False)
+    df.to_csv(f'{path}\\{name}.csv', index=False)
     print('Save files in:')
-    print(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv')
-    print(f'{path}/{name}.csv')
+    print(f'{path}\\{name}\\{name}_{dt.strftime("%Y_%m_%d")}.csv')
+    print(f'{path}\\{name}.csv')
     
     
 def pl_to_csv(df, path, name):
     dt = datetime.now()
-    df.write_csv(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv')
-    df.write_csv(f'{path}/{name}.csv')
+    df.write_csv(f'{path}\\{name}\\{name}_{dt.strftime("%Y_%m_%d")}.csv')
+    df.write_csv(f'{path}\\{name}.csv')
     print('Save files in:')
-    print(f'{path}/{name}/{name}_{dt.strftime("%Y_%m_%d")}.csv')
-    print(f'{path}/{name}.csv')
+    print(f'{path}\\{name}\\{name}_{dt.strftime("%Y_%m_%d")}.csv')
+    print(f'{path}\\{name}.csv')
